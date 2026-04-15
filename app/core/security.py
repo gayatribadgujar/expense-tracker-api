dbb2b83cb4338ac5819ad3c8eb5c0218c298568e
Gayatri Badgujar <gayatribadgujar1995@gmail.com>
2026-04-15 19:22:37 +0530

    code changes

--- app/core/security.py
+++ app/core/security.py
@@ -12,14 +12,12 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
+    return pwd_context.hash(password[:72])
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     try:
-        return pwd_context.verify(plain_password, hashed_password)
+        return pwd_context.verify(plain_password[:72], hashed_password)
     except Exception:
         return False
 
